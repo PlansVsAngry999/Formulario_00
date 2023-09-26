0716ee032a01d27295fd6e0b08f21a6481d24b3f
PlansVsAngry999 <plvsan999@gmail.com>
2023-09-26 16:38:32 -0600

    Añadir proyecto... LOL

---
+++ Formulario_00/src/formulario_00/Formulario_00.java
@@ -0,0 +1,18 @@
+package formulario_00;
+/**
+ * Programa que0
+ * @author Ricardo Daniel Hernández Sosa (PlansVsAngry999).
+ */
+import javax.swing.*;
+public class Formulario_00 extends JFrame{
+    public Formulario_00(){
+        setLayout(null);
+    }
+    public static void main(String[] args) {
+     Formulario_00 objeto = new Formulario_00();
+     objeto.setBounds(10,20,400,300);
+     objeto.setVisible(true);
+     objeto.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
